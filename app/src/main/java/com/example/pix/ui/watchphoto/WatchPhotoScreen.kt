e6d6016050abb0d6e33d3edd0dd31561c996638a
mk1MoreBugs <o.v.klevtsov@ya.ru>
2025-04-06 01:57:56 +0300

    implement pinch-to-zoom

--- app/src/main/java/com/example/pix/ui/watchphoto/WatchPhotoScreen.kt
+++ app/src/main/java/com/example/pix/ui/watchphoto/WatchPhotoScreen.kt
@@ -1,15 +1,21 @@
 package com.example.pix.ui.watchphoto
 
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.pix.R
-import com.example.pix.domain.entity.Picture
 import com.example.pix.ui.components.PixScaffold
 
 @Composable
@@ -34,8 +40,18 @@ fun WatchPhotoScreenContent(
     pictureUrl: String,
     pictureTitle: String
 ) {
+    var scale by remember { mutableStateOf(1f) }
     AsyncImage(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize()
+            .graphicsLayer(
+                scaleX = scale,
+                scaleY = scale
+            )
+            .pointerInput(Unit) {
+                detectTransformGestures { _, _, zoom, _ ->
+                    scale = (scale * zoom).coerceIn(0.5f, 3f)
+                }
+            },
         model = ImageRequest.Builder(LocalContext.current)
             .data(pictureUrl)
             .crossfade(true)
