444bf41b730b52d3f762909f4c0c44a79c34acf2
mk1MoreBugs <o.v.klevtsov@ya.ru>
2025-04-05 12:09:25 +0300

    add logger to retrofit

--- app/src/main/java/com/example/pix/data/flickr/FlickrRetrofit.kt
+++ app/src/main/java/com/example/pix/data/flickr/FlickrRetrofit.kt
@@ -1,13 +1,22 @@
 package com.example.pix.data.flickr
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object FlickrRetrofit {
+    private val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    private val client: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(interceptor).build()
+
     fun create(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://www.flickr.com")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
     }
 }
